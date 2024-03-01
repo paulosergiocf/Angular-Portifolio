a6ccddf51f42e810807d2e1d61b9b67593a24fad
Paulo Sergio <paulosergiocf.dev@gmail.com>
2024-02-29 23:13:38 -0300

    update data

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -4,8 +4,7 @@ export const perfilData: Perfil = {
   capa:"https://paulosergiocf.github.io/portfolio/img/undraw_developer_activity_re_39tg(1).svg",
   nome: 'Paulo Sergio de Campos Filho',
   atividade: 'Desenvolvedor de Software',
-  sobre:
-    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas reprehenderit inventore culpa consectetur repudiandae ullam ipsa? Reprehenderit dolore, eaque consequatur, laboriosam maxime modi quidem maiores vero repellat natus voluptate molestiae.',
+  sobre:"Sou desenvolvedor e atuo com diversas tecnologias, incluindo Java, Python, JSF e banco de dados. Desde o início da minha carreira, tenho buscado atualizar e aprimorar minhas habilidades, sempre mirando a excelência no desenvolvimento de soluções eficientes e de qualidade. Sou apaixonado por tecnologia e tenho uma forte habilidade em resolução de problemas, o que me permite encontrar soluções criativas para desafios técnicos.",
   contatos:{
     telefone:"15 99846-2920",
     email:"paulosergiocf.dev@gmail.com",
