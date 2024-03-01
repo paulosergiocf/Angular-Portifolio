bd9bde082b08ea7c15eff84bd65f402f749e7a7a
Paulo Sergio <paulosergiocf.dev@gmail.com>
2024-02-29 23:23:50 -0300

    ajustes nos dados

--- src/data/projetos.ts
+++ src/data/projetos.ts
@@ -5,7 +5,7 @@ export const projetosData: Projeto[] = [
     id: '1',
     titulo: 'Download Musicas Youtube',
     imagem:
-      'https://private-user-images.githubusercontent.com/49497668/308407765-db2e6efc-dfa1-488f-9f81-28605f6aae0d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDkyMjA4MDcsIm5iZiI6MTcwOTIyMDUwNywicGF0aCI6Ii80OTQ5NzY2OC8zMDg0MDc3NjUtZGIyZTZlZmMtZGZhMS00ODhmLTlmODEtMjg2MDVmNmFhZTBkLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAyMjklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMjI5VDE1MjgyN1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTg3NzQ5Y2Q3ZTFmNzA5NWEzNGUxZTFjZjZmN2M1Y2QyNTlmZmRjNjNlZDVjYmNkNGI4NzNiZTUyOWVhZmYxYWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.c0afi_Ve-u_woyNoK-wohxYRggoke8z91FOVfilaINc',
+      '../../assets/img/308407765-db2e6efc-dfa1-488f-9f81-28605f6aae0d.png',
     descricao:
       'O que motivou a criação dessa ferramenta foi a necessidade de adicionar musicas Mp3 a um pendrive para poder ouvir no carro. Com o avanço das ferramentas de streaming houve uma queda em quantiade e qualidade nas ferramentas de downloads, principalmente para downloads em massa, quando surgiu a demanda, percebi que as ferramentas que utilizava ou estava defasadas e deixaram de funcionar ou passaram a cobrar pelo serviço.',
     stacks: [
@@ -41,8 +41,10 @@ export const projetosData: Projeto[] = [
   {
     id: '3',
     titulo: 'Amigo Secreto',
-    imagem: '../../assets/img/Imagem colada 1.png',
-    descricao: 'Aplicação Web para gerenciar a sortear amigos secretos. aplicação tem o objetivo de tornar facil se segura os entros de fim de ano.',
+    imagem:
+      'https://github.com/paulosergiocf/Portifolio/blob/main/src/assets/img/Imagem%20colada%201.png?raw=true',
+    descricao:
+      'Aplicação Web para gerenciar a sortear amigos secretos. aplicação tem o objetivo de tornar facil se segura os entros de fim de ano.',
     stacks: [
       {
         nome: 'python',
